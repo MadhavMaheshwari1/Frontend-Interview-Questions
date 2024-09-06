5546763e1c749b63c68a4eda30b4855d02b94b25
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-09-06 10:45:21 +0530

    Almost Done

--- Pagination/src/components/HomePage.jsx
+++ Pagination/src/components/HomePage.jsx
@@ -6,8 +6,8 @@ const HomePage = ({ itemsPerPage }) => {
 
   const [products, setProducts] = useState([]);
   const [buttons, setButtons] = useState([]);
-  const [startButtonRange, setStartButtonRange] = useState(1);
-  const [endButtonRange, setEndButtonRange] = useState(4);
+  const [startButtonRange, setStartButtonRange] = useState(0);
+  const [endButtonRange, setEndButtonRange] = useState(3);
 
   const [startRange, setStartRange] = useState(1);
   const [endRange, setEndRange] = useState(itemsPerPage);
@@ -22,13 +22,32 @@ const HomePage = ({ itemsPerPage }) => {
     }
   }
 
+  const pageHelper = (next) => {
+    if (next) {
+      setEndButtonRange((endButtonRange) => endButtonRange + 1);
+      setStartButtonRange((startButtonRange) => startButtonRange + 1);
+    }
+    else {
+      setEndButtonRange((endButtonRange) => endButtonRange - 1);
+      setStartButtonRange((startButtonRange) => startButtonRange - 1);
+    }
+  }
+
+  const pageHandler = (buttonNumber) => {
+    setStartRange(buttonNumber * itemsPerPage + 1);
+    setEndRange((buttonNumber + 1) * (itemsPerPage));
+  }
+
   useEffect(() => {
     fetchProducts();
   }, [])
 
   useEffect(() => {
-    // Update `buttons` whenever `products` is updated
-    setButtons(Array.from({ length: products.length }, (_, index) => index + 1));
+    pageHandler(endButtonRange);
+  }, [startButtonRange, endButtonRange])
+
+  useEffect(() => {
+    setButtons(Array.from({ length: Math.ceil(products.length / itemsPerPage) }, (_, index) => index));
   }, [products]);
 
   if (products.length === 0) {
@@ -36,13 +55,15 @@ const HomePage = ({ itemsPerPage }) => {
   }
 
   return (
-    <div className='min-h-[100vh] h-auto max-w-[1000px] mx-auto mt-4'>
+    <div className='min-h-[100vh] h-auto max-w-[1000px] mx-auto mt-4 py-10'>
       {
         products.map((product) =>
         ((product.id >= startRange && product.id <= endRange) ?
           (
-            <div className="flex gap-4 items-center" key={product.id}>
-              <img src={product.images[0]} className="h-[200px]" alt="" />
+            <div className="flex gap-4 items-center shadow-lg mb-4 px-4" key={product.id}>
+              <div className="w-1/3 flex justify-center items-center">
+                <img src={product.images[0]} className="h-[200px] object-cover " alt="" />
+              </div>
               <div>
                 <h1>Name: {product.title}</h1>
                 <p>Description: {product.description}</p>
@@ -52,14 +73,18 @@ const HomePage = ({ itemsPerPage }) => {
           : null)
         )
       }
-      <div className='flex justify-center'> {
-        buttons.map((button) => (
-          (button >= startButtonRange && button <= endButtonRange) ? (
-            <button key={button} className='py-2 px-2 text-4xl'>{button}</button>
-          ) : null
-        ))
-      }
-
+      <div className='flex justify-center py-8'>
+        {startButtonRange !== 0 ? <button className='text-3xl mr-4' onClick={() => pageHelper(false)}>Prev</button> : null}
+        {
+          buttons.map((button) => (
+            (button >= startButtonRange && button <= endButtonRange) ? (
+              <div key={button}>
+                <button className='py-2 px-2 text-4xl bg-gray-200 hover:bg-blue-500 mr-4' onClick={() => pageHandler(button)}>{button + 1}</button>
+              </div>
+            ) : null
+          ))
+        }
+        {endRange !== products.length ? <button className='text-3xl' onClick={() => pageHelper(true)}>Next</button> : null}
       </div>
     </div>
   )
