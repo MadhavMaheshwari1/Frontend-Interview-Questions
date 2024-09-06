41018f3baa203206af98f6542da6173e9dc67511
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-09-06 08:30:06 +0530

    Got the data

--- Pagination/src/components/HomePage.jsx
+++ Pagination/src/components/HomePage.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-const HomePage = ({itemsPerPage}) => {
+const HomePage = ({ itemsPerPage }) => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await axios.get('https://dummyjson.com/products?limit=0');
+    setProducts(response.data.products);
+    console.log(response.data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
+  if (products.length === 0) {
+    return <div className='text-6xl font-bold'>Loading...</div>
+  }
+
   return (
-    <div>HomePage</div>
+    <div className='h-[100vh]'>{
+      products.map((product) =>
+        <div key={product.id}>{product.id}</div>
+      )
+    }
+    </div>
   )
 }
 
